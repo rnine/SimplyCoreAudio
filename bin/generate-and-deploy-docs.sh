04c4ab5ba6d52c7ac05f7f998a380e13d7784823
Ruben Nine <ruben@9labs.io>
2016-11-11 13:18:52 +0100

    Adding github_url.

--- bin/generate-and-deploy-docs.sh
+++ bin/generate-and-deploy-docs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
-jazzy --xcodebuild-arguments -project,AMCoreAudio.xcodeproj
+jazzy --github_url https://github.com/rnine/AMCoreAudio --xcodebuild-arguments -project,AMCoreAudio.xcodeproj
 GIT_DEPLOY_DIR=docs GIT_DEPLOY_BRANCH=gh-pages GIT_DEPLOY_REPO=git@github.com:rnine/AMCoreAudio.git ./bin/deploy-docs.sh
