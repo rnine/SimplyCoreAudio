eeca8e4d235e881d1caefa81f9c66c6ca6784317
Ruben Nine <ruben@9labs.io>
2021-03-21 09:11:25 +0100

    Adding synchronized access to AudioObjectPool get, set and delete functions.

--- Source/Internal/AudioObjectPool.swift
+++ Source/Internal/AudioObjectPool.swift
@@ -10,6 +10,8 @@ class AudioObjectPool {
     // MARK: - Private Properties
 
     private let pool: NSMapTable<NSNumber, AudioObject> = NSMapTable.weakToWeakObjects()
+    private lazy var queueLabel = Bundle(for: type(of: self)).bundleIdentifier!.appending(".audioObjectPool")
+    private lazy var queue = DispatchQueue(label: queueLabel, qos: .default, attributes: .concurrent)
 
     // MARK: - Static Properties
 
@@ -24,22 +26,27 @@ class AudioObjectPool {
 
 extension AudioObjectPool {
     func get(_ id: UInt32) -> AudioObject? {
-        pool.object(forKey: NSNumber(value: id))
+        queue.sync {
+            pool.object(forKey: NSNumber(value: id))
+        }
     }
 
     func set(_ audioObject: AudioObject, for id: UInt32) {
-        pool.setObject(audioObject, forKey: NSNumber(value: id))
+        queue.sync(flags: .barrier) {
+            pool.setObject(audioObject, forKey: NSNumber(value: id))
+        }
     }
 
     @discardableResult
     func remove(_ id: UInt32) -> Bool {
-        let key = NSNumber(value: id)
+        queue.sync(flags: .barrier) {
+            let key = NSNumber(value: id)
 
-        if pool.doesContain(key) {
+            guard pool.doesContain(key) else { return false }
+
             pool.removeObject(forKey: key)
+
             return true
-        } else {
-            return false
         }
     }
 }
