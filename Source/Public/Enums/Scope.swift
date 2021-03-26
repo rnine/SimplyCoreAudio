5c52991a02d4ba617c338e2cfcc70f1e2aa12ed6
Ruben Nine <ruben@9labs.io>
2021-03-26 08:01:19 +0100

    Minor edit.

--- Source/Public/Enums/Scope.swift
+++ Source/Public/Enums/Scope.swift
@@ -9,7 +9,7 @@ import Foundation
 
 /// Indicates the scope used by an `AudioDevice` or `AudioStream`.
 ///
-/// Please notice that `AudioStream` only supports `input` or `output` scopes,
+/// Please notice that `AudioStream` only supports `input` and `output` scopes,
 /// whether as `AudioDevice` may, additionally, support `global` and `playthrough`.
 public enum Scope {
     /// Global scope
