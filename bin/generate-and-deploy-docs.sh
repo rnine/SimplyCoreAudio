89e973969bf4f022f34c073955b8733cf0efaec2
Ruben Nine <ruben@9labs.io>
2022-09-24 10:40:02 +0200

    Removed doc generation (jazzy) & deploying scripts.

--- bin/generate-and-deploy-docs.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit #abort if any command fails
-jazzy
-GIT_DEPLOY_DIR=docs GIT_DEPLOY_BRANCH=gh-pages GIT_DEPLOY_REPO=git@github.com:rnine/SimplyCoreAudio.git ./bin/deploy-docs.sh
